deabb7c10101704c1c2068bd4b86ef1e2cc23ede
toniher <toniher@cau.cat>
2016-09-20 13:17:43 +0200

    update SMWQueryTag

---
+++ SMWQueryTag.i18n.magic.php
@@ -0,0 +1,12 @@
+<?php
+/**
+* Internationalization file.
+*/
+ 
+$magicWords = array();
+ 
+$magicWords['en'] = array(
+	'SMWQueryTag' => array( 0, 'SMWQueryTag' ),
+	'SMWQueryList' => array( 0, 'SMWQueryList' )
+);
+
